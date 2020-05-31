18ec0122cd602ca821b29ba43568ee737647541b
Christopher <c.d.ross1989@icloud.com>
2020-05-31 16:23:19 -0500

    Change Map

--- scripts/map.js
+++ scripts/map.js
@@ -2,10 +2,23 @@ var mymap = L.map("mapid").setView([27.6648, -84.5158], 6);
 var mapboxAccessToken =
   "pk.eyJ1IjoiY3Jvc3MxOTg5IiwiYSI6ImNrYW82bDlueTBjYXAydW80cmNpejN2YjIifQ.TkqWhxj4bjvQgv1H3TPE1Q";
 
-L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+/*L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(mymap);
+}).addTo(mymap);*/
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" +
+    mapboxAccessToken,
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/light-v10",
+    tileSize: 512,
+    zoomOffset: -1,
+  }
+).addTo(mymap);
 
 function style(feature) {
   return {
